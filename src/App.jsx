4e7185af4b4865bb4293a50728195b5d9a8c11a5
sebasdurango <sebasdurango19@gmail.com>
2024-07-13 15:01:48 -0500

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  //useEffect se ejecuta despues de cada renderizado
+  useEffect(()=>{
+    //Actualiza el Titulo del documento con el count(pestaña de la pagina)
+    document.title = `Count is ${count}`;
+  }, [count]);
+
   return (
     <>
       <h1>React Hooks Practices</h1>
       <div className="card">
+        <hr />
         <h2>Excercise Hook useState</h2>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <hr />
       </div>
     </>
   )

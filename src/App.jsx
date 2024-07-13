b0bb7ad6621bdb74c5ca255dc1c4546361183c8f
sebasdurango <sebasdurango19@gmail.com>
2024-07-13 14:52:16 -0500

    Hook useState

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>React Hooks Practices</h1>
+      <div className="card">
+        <h2>Excercise Hook useState</h2>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
